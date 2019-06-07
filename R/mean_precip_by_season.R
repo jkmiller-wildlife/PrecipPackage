71ee273827a38120ccc888cbe94328d4c83a5427
Anna Calle <annagcalle@bren.ucsb.edu>
2019-06-07 10:39:29 -0700

    Commit attempt

---
+++ R/mean_precip_by_season.R
@@ -0,0 +1,32 @@
+#' Mean Precipitation per Season
+#'
+#' This function calculates the mean precipation by season
+#' @param inputs/parameter description
+#' @return what your function returns (outputs)
+#' @example how to use it
+#' @references citations or urls
+#' @author Jamie Miller and Anna Calle
+#'
+
+mean_precip_by_season = function(month, precip) {
+  mean <- month %>%
+    group_by()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

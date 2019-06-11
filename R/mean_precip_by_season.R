8a3e612eee5e38e7a130d39e8e5d4fee4bb50954
Anna Calle <annagcalle@bren.ucsb.edu>
2019-06-10 21:37:59 -0700

    Created mean precip by season function

--- R/mean_precip_by_season.R
+++ R/mean_precip_by_season.R
@@ -1,16 +1,34 @@
 #' Mean Precipitation per Season
 #'
-#' This function calculates the mean precipation by season
-#' @param inputs/parameter description
+#' This function calculates the mean precipation by season for a given year
+#' @param precip_data data frame that contains the following information
 #' @return what your function returns (outputs)
 #' @example how to use it
 #' @references citations or urls
 #' @author Jamie Miller and Anna Calle
 #'
 
-mean_precip_by_season = function(month, precip) {
-  mean <- month %>%
-    group_by()
+mean_precip_by_season = function(precip_data, year) {
+mean <- precip_data %>%
+  filter(water_year == year) %>%
+  mutate( Season = case_when(
+    month == "JAN" ~ "Winter",
+    month == "FEB" ~ "Winter",
+    month == "MAR" ~ "Spring",
+    month == "APR" ~ "Spring",
+    month == "MAY" ~ "Spring",
+    month == "JUN" ~ "Summer",
+    month == "JUL" ~ "Summer",
+    month == "AUG" ~ "Summer",
+    month == "SEP" ~ "Fall",
+    month == "OCT" ~ "Fall",
+    month == "NOV" ~ "Fall",
+    month == "DEC" ~ "Winter"
+  )) %>%
+    group_by(Season) %>%
+    summarize("Mean Precipitation" = round(mean(precip), digits = 3))
+
+  return(mean)
 }
 
 

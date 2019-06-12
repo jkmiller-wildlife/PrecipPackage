b0f3fc097dcdac415b53dea184c68270d8c277b6
jkmiller-wildlife <jkmiller.wildlife@gmail.com>
2019-06-12 11:53:21 -0700

    start function for total precip by water year

---
+++ R/total_precip_by_year.R
@@ -0,0 +1,12 @@
+#' Total Precipitation for a each water year
+#'
+#' This function calculates the mean precipation by season for a given year
+#' @param precip_data data frame that contains the following information
+#' @return what your function returns (outputs)
+#' @example how to use it
+#' @references citations or urls
+#' @author Jamie Miller and Anna Calle
+#'
+
+
+

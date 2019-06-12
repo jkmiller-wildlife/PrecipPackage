5f030572257ca38706c4ae07cc0f28252f5f1037
jkmiller-wildlife <jkmiller.wildlife@gmail.com>
2019-06-12 16:32:15 -0700

    added a graph and kable table to mean_precip_by_month function from array

--- R/mean_precip_by_month.R
+++ R/mean_precip_by_month.R
@@ -9,11 +9,35 @@
 #'
 
 
-mean_precip_by_month <- function(precip_array) {
+mean_precip_by_month <- function(precip_array, plot = TRUE) {
 
   precip_month_mean <- apply(precip_array, MARGIN = 3, FUN = mean)
 
-  return(precip_month_mean)
+  mean_precip_by_month_df <- data.frame(month = c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"),
+                                        mean_precip = precip_month_mean)
+
+  if(plot == TRUE) {
+
+    mean_precip_by_month_df$month = factor(mean_precip_by_month_df$month, levels = month.abb)
+
+    mean_precip_by_month_plot <- ggplot(mean_precip_by_month_df, aes(x = month, y = mean_precip)) +
+      geom_col() +
+      labs(x = "Month", y = "Mean Precipitation (inches)") +
+      scale_x_discrete(limits = month.abb) +
+      theme_bw()
+    print(mean_precip_by_month_plot)
+  }
+
+  if(plot == FALSE) {
+
+    mean_precip_by_month_table <- mean_precip_by_month_df %>%
+      rename("Month" = month,
+             "Mean Precipitation (inches)" = mean_precip) %>%
+      knitr::kable(digits = 2)
+  }
+
+
+  return(mean_precip_by_month_table)
 
 }
 

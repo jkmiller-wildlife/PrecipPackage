592b7f78d7cfa02ce029da1a898ad70df90b0d2e
jkmiller-wildlife <jkmiller.wildlife@gmail.com>
2019-06-13 15:01:29 -0700

    4 of 5 functions tested and work.

--- R/total_precip_by_year.R
+++ R/total_precip_by_year.R
@@ -9,30 +9,35 @@
 #'
 
 
-total_precip_by_year <- function(precip_data, plot = FALSE) {
+total_precip_by_year <- function(precip_data, plot = TRUE) {
 
   wy_tot <- precip_data %>%
     group_by(Location, water_year) %>%
     summarize(sum_precip = sum(precip)) %>%
     ungroup()
 
-  if(plot == TRUE) {
-
     wy_tot_plot <- ggplot(wy_tot, aes(x = water_year, y = sum_precip)) +
       geom_col(aes(fill = Location)) +
       labs(x = "Water Year", y = "Total Precipitation (inches)") +
       theme_bw()
-    print(wy_tot_plot)
-  }
-
-  if(plot == FALSE) {
 
     wy_tot_table <- wy_tot %>%
       rename("Water Year" = water_year) %>%
       spread(wy_tot, key = Location, value = sum_precip) %>%
       knitr::kable()
+
+    if (plot == TRUE) {
+
+      return(list(wy_tot_table,wy_tot_plot))
+
     }
 
-  return(wy_tot_table)
+    if (plot == F) {
+
+      return(list(wy_tot_table))
+
+    }
+
 
   }
+
